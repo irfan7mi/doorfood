b63facdb6ddf0445c08c5848c1a459ec48259688
irfan <mi2268242@gmail.com>
2024-12-31 20:42:57 +0530

    server vercelup

--- Client/context/Context.jsx
+++ Client/context/Context.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext (null)
 const StoreContextProvider = (props) => {
     const [image,setImage] = useState(true)
     const [data, setData] = useState([])
-    const url = "https://doorfood-app-server-98ey71h4u-irfans-projects-878c5a63.vercel.app/"
+    const url = "https://doorfood-app-server-98ey71h4u-irfans-projects-878c5a63.vercel.app"
 
 const contextValue = {
     image,
