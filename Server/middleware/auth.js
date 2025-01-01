e0226ad4f134c398e02bdbc01503cfae4dcd912d
irfan <mi2268242@gmail.com>
2025-01-01 22:30:43 +0530

    server vercelup

--- Server/middleware/auth.js
+++ Server/middleware/auth.js
@@ -1,22 +1,8 @@
 import jwt from 'jsonwebtoken';
 
 const authMiddleware = async (req, res, next) => {
-  // Retrieve the token from the Authorization header
-  const authHeader = req.headers.Authorization;
-  if (authHeader || !authHeader.startsWith('Bearer ')) {
-    return res.status(401).json({ success: false, message: 'Not Authorized. Login Again!' });
-  }
-
-  const token = authHeader.split(' ')[1]; // Extract the token from "Bearer <token>"
 
   try {
-    // Verify the token
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    
-    // Attach the decoded user information (e.g., id) to the request for further use
-    req.user = decoded;
-
-    // Proceed to the next middleware or route handler
     next();
   } catch (error) {
     console.error('Token verification error:', error);
